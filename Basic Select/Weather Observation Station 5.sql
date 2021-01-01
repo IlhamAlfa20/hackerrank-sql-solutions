3a3f3a7423dbe5642764338cedf7149cb1191df8
Ilham Alfaridho <ilhamalfa20@gmail.com>
2021-01-01 15:19:23 +0700

    add Weather Observation Station 4 & 5

---
+++ Basic Select/Weather Observation Station 5.sql
@@ -0,0 +1,9 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+SELECT city, LENGTH(city) 
+FROM station 
+ORDER BY LENGTH(city) ASC, city ASC LIMIT 1;
+
+SELECT DISTINCT(city), LENGTH(city) 
+FROM station 
+ORDER BY LENGTH(city) DESC, city ASC LIMIT 1;
